53ef95fcc4166f8e00741ea2bb0dea5c3036152d
alhxmbs <alhexmbs@gmail.com>
2024-12-15 23:33:19 -0500

    Primeros pasos

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>Bienvenido</h1>
+      <p>Test desplegando</p>
+    </div>
+  )
+}
+
+export default MyComponent
