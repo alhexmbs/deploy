698087aebf47878934f3a5abe2df347ac73b5c31
alhxmbs <alhexmbs@gmail.com>
2024-12-16 00:07:47 -0500

    Probando imagen

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import './App.css'
 
 const MyComponent = () => {
+  const rutaImagen = '../public/cyberpunk.jpg'
+
   return (
     <div>
-      <img src="../cyberpunk.jpg" alt="imagenHero" id="imagenHero" />
+      <img src={ rutaImagen } alt="imagenHero" id="imagenHero" />
       <h1>🦖 Bienvenido ✌️</h1>
       <p> <strong><i>Despliegue termiando </i>🚀</strong> </p>
 
